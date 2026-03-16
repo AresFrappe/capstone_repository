a0cb8098938a5b0e77bcb842749f6e6a81f19eef
AresFrappe <aariessapin05@gmail.com>
2026-03-16 20:24:38 +0800

    initial commit

---
+++ app/routes.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from . import mongo
+
+main = Blueprint("main", __name__)
+
+@main.route("/")
+def home():
+
+    capstones = mongo.db.capstones.find()
+
+    return render_template(
+        "index.html",
+        capstones=capstones
+    )
+
+
+@main.route("/add", methods=["POST"])
+def add_capstone():
+
+    title = request.form.get("title")
+    author = request.form.get("author")
+
+    mongo.db.capstones.insert_one({
+        "title": title,
+        "author": author
+    })
+
+    return redirect(url_for("main.home"))
